447661b7e251426e06e6c4a527f85de7f2bc50f4
Tamer Yetim <tameryetim@gmail.com>
2020-11-09 14:33:28 +0300

    Update SiteConfig.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -9,13 +9,14 @@ const config = {
   faviconUrl: '../content/images/favicon.ico',
   repo: 'https://github.com/tameryetim/personalsite',
   pathPrefix: '',
+   dateFromFormatNew: 'YYYY',
   // dateFromFormat: 'YYYY-MM-DD',
   dateFormat: 'YYYY-MM-DD',
   siteDescription:
     'Hello, this\'s Tamer Yetim.',
   siteRss: '/rss.xml',
-  googleAnalyticsID: 'G-8N23RZ672S',
-  // userName: 'Charles',
+  googleAnalyticsID: 'UA-48316511-1',
+  // userName: '',
   // userEmail: '',
   // userTwitter: '',
   menuLinks: [
@@ -33,22 +34,17 @@ const config = {
       link: '/me/',
     },
   ],
-  themeColor: '#3F80FF', // Used for setting manifest and progress theme colors.
+  themeColor: '#3F80FF', 
   backgroundColor: '#ffffff',
 }
 
-// Make sure pathPrefix is empty if not needed
 if (config.pathPrefix === '/') {
   config.pathPrefix = ''
 } else {
-  // Make sure pathPrefix only contains the first forward slash
   config.pathPrefix = `/${config.pathPrefix.replace(/^\/|\/$/g, '')}`
 }
-
-// Make sure siteUrl doesn't have an ending forward slash
 if (config.siteUrl.substr(-1) === '/') config.siteUrl = config.siteUrl.slice(0, -1)
 
-// Make sure siteRss has a starting forward slash
 if (config.siteRss && config.siteRss[0] !== '/') config.siteRss = `/${config.siteRss}`
 
 module.exports = config
