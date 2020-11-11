75592837840f071a9a78cead2dee9ade51332ff7
Tamer Yetim <tameryetim@gmail.com>
2020-11-11 22:04:15 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -60,7 +60,14 @@ export default class Index extends Component {
       </section>
         <div className="container front-page">
 
- <div align="center">
+ <!-- Begin Mailchimp Signup Form -->
+          <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+          <style type="text/css">
+              #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+              /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+                 We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+          </style>
+          <div id="mc_embed_signup">
               <form action="https://tameryetim.us2.list-manage.com/subscribe/post?u=51aac1edce58c2fbd21e62975&amp;id=4d36626e0b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                   <div id="mc_embed_signup_scroll">
                       <h2>E-Mail Listesi</h2>
@@ -77,6 +84,7 @@ export default class Index extends Component {
                   </div>
               </form>
           </div>
+
 
 
 
