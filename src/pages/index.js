aeb4b962ff3bb85d47782e19e8b56148b84df765
Tamer Yetim <tameryetim@gmail.com>
2020-11-11 22:01:51 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,6 +59,28 @@ export default class Index extends Component {
         </div>
       </section>
         <div className="container front-page">
+
+ <div align="center">
+              <form action="https://tameryetim.us2.list-manage.com/subscribe/post?u=51aac1edce58c2fbd21e62975&amp;id=4d36626e0b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                  <div id="mc_embed_signup_scroll">
+                      <h2>E-Mail Listesi</h2>
+                      <div class="mc-field-group">
+                          <label for="mce-EMAIL">Email Adresiniz </label>
+                          <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                      </div>
+                      <div id="mce-responses" class="clear">
+                          <div class="response" id="mce-error-response" style="display:none"></div>
+                          <div class="response" id="mce-success-response" style="display:none"></div>
+                      </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_51aac1edce58c2fbd21e62975_4d36626e0b" tabindex="-1" value=""></div>
+                      <div class="clear"><input type="submit" value="KAYDOL" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                  </div>
+              </form>
+          </div>
+
+
+
+
           <section className="section">
             <h2>
               Son İçerikler
