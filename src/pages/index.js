c73a9d85a09a95d8289ff41b2a00010a1a13dc32
Tamer Yetim <tameryetim@gmail.com>
2020-12-21 01:11:09 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,9 +61,7 @@ export default class Index extends Component {
         <div className="container front-page">
 
       <center>
-      <a href='https://www.bossbasketballmanager.com' target='_blank'><img src='https://www.bossbasketballmanager.com/dosyalar/gif.gif' style='  max-width: 100%;
-  height: auto;
-  width: auto\9;' /></a>
+      <a href='https://www.bossbasketballmanager.com' target='_blank'><img src='https://www.bossbasketballmanager.com/dosyalar/gif.gif' style='max-width: 100%; width: auto; height: auto;' /></a>
       </center>
 
 
